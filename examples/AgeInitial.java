cc5b5f85e537abe88d1668dcb73917c5794b0c9e
SvenSkoglund <32058317+SvenSkoglund@users.noreply.github.com>
2017-09-20 20:19:51 +0700

    Codes

---
+++ examples/AgeInitial.java
@@ -0,0 +1,11 @@
+package examples;
+
+public class AgeInitial{
+
+public static void main (String[]Args){
+	int age = 25;
+	char mi = 'R';
+	
+	System.out.println("Sven" + mi + "Skoglund: Age:" + age);
+}
+}
