87e35e1a6a50a064fdc184320ce48a7707790c49
SvenSkoglund <32058317+SvenSkoglund@users.noreply.github.com>
2017-09-20 20:27:32 +0700

    more code

---
+++ examples/StudentScores.java
@@ -0,0 +1,25 @@
+package examples;
+
+public class StudentScores {
+    public static void main(String[] args) {
+	
+	
+		int score1 = 95;
+		int score2 = 51;
+		int score3 = 12;
+		int score4 = 90;
+		
+		float average = (score1+score2+score3+score4) / 4;
+		
+		System.out.println("Score one is: " + score1 +"\n");
+		
+		System.out.println("Score two is: " + score2 +"\n");
+		
+		System.out.println("Score three is: " + score3 +"\n");
+		
+		System.out.println("Score four is: " + score3 +"\n");
+		
+		
+		System.out.println("The average score is: " + average);
+}
+}
